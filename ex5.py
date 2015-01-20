7b7ece75257e291b2a3a31be6d78541c2178651f
Teri Charles <tericharles@juno.com>
2015-01-20 15:26:36 -0700

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,15 @@
+my_name = 'Teri Charles'
+my_age = 3
+my_height = 32 #inches
+my_weight = 10
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall" % my_height
+print "She's %d pounds heavy" % my_weight
+print "She's got %s eyes and %s hair" % (my_eyes, my_hair)
+print "Her teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d, %d, and %d I get %d. " % (my_age, my_height, my_weight, my_age + my_height + my_weight)
