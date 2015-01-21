3f36215e12083db56f39c95db2945c139cd0d6c1
Teri Charles <tericharles@juno.com>
2015-01-21 09:56:31 -0700

    Update ex25.py
    Still learning Git/GitHub, so this is where I tried to edit this file for the results.

--- ex25.py
+++ ex25.py
@@ -55,6 +55,43 @@ ex25.print_first_and_last_sorted(sentence)
 '''
 
 '''
+This should be the results:
+
+Python 2.7.1 (r271:86832, Jun 16 2011, 16:59:05) 
+[GCC 4.2.1 (Based on Apple Inc. build 5658) (LLVM build 2335.15.00)] on darwin
+Type "help", "copyright", "credits" or "license" for more information.
+>>> import ex25
+>>> sentence = "All good things come to those who wait."
+>>> words = ex25.break_words(sentence)
+>>> words
+['All', 'good', 'things', 'come', 'to', 'those', 'who', 'wait.']
+>>> sorted_words = ex25.sort_words(words)
+>>> sorted_words
+['All', 'come', 'good', 'things', 'those', 'to', 'wait.', 'who']
+>>> ex25.print_first_word(words)
+All
+>>> ex25.print_last_word(words)
+wait.
+>>> words
+['good', 'things', 'come', 'to', 'those', 'who']
+>>> ex25.print_first_word(sorted_words)
+All
+>>> ex25.print_last_word(sorted_words)
+who
+>>> sorted_words
+['come', 'good', 'things', 'those', 'to', 'wait.']
+>>> sorted_words = ex25.sort_sentence(sentence)
+>>> sorted_words
+['All', 'come', 'good', 'things', 'those', 'to', 'wait.', 'who']
+>>> ex25.print_first_and_last(sentence)
+All
+wait.
+>>> ex25.print_first_and_last_sorted(sentence)
+All
+who
+'''
+
+'''
 Also in the python interpreter, try doing this: help(ex25) and also help(ex25.break_words). 
 Notice how you get help for your module, and how the help is those odd """ strings you put 
 after each function in ex25? Those special strings are called documentation comments and 
